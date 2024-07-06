77e3d81167fcbca5251b6ff52357650a9b12e16c
Gary <reid9000200020002000@gmail.com>
2024-07-06 21:34:20 +0800

    first commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import LoginForm from "@/components/auth/login-form";
+
+type Props = {};
+
+export default function LoginPage({}: Props) {
+  return <LoginForm />;
+}
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export default function UsersPage({}: Props) {
+  return <div>Users Page</div>;
+}
