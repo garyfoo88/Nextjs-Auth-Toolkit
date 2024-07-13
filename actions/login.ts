61d583f87bda6406bf1bd5961ae52a83ee61fef9
Gary <reid9000200020002000@gmail.com>
2024-07-13 19:07:42 +0800

    Implement logged-in navbar

--- actions/login.ts
+++ actions/login.ts
@@ -17,69 +17,74 @@ import { z } from "zod";
 export const login = async (
   values: z.infer<typeof LoginSchema>
 ): Promise<any> => {
-  const validateFields = LoginSchema.safeParse(values);
-  if (validateFields.error) return { error: "Invalid fields" };
+  try {
+    const validateFields = LoginSchema.safeParse(values);
+    if (validateFields.error) return { error: "Invalid fields" };
 
-  const { email, password, code } = validateFields.data;
+    const { email, password, code } = validateFields.data;
 
-  const existingUser = await getUserByEmail(email);
+    const existingUser = await getUserByEmail(email);
 
-  if (!existingUser || !existingUser.email) {
-    return { error: "Email does not exist" };
-  }
+    if (!existingUser || !existingUser.email) {
+      return { error: "Email does not exist" };
+    }
 
-  if (!existingUser.emailVerified) {
-    const { email, token } = await generateVerificationToken(
-      existingUser.email
-    );
-
-    await sendVerificationEmail(email, token);
-    return { success: "Confirmation email sent!" };
-  }
-
-  if (existingUser.isTwoFactorEnabled && existingUser.email) {
-    if (code) {
-      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
-
-      if (!twoFactorToken || twoFactorToken.token !== code)
-        return { error: "Invalid code!" };
-
-      const hasExpired = new Date(twoFactorToken.expiresAt) < new Date();
-
-      if (hasExpired)
-        return {
-          error: "Code expired!",
-        };
-
-      await db.twoFactorToken.delete({
-        where: {
-          id: twoFactorToken.id,
-        },
-      });
-
-      const existingConfirmation = await getTwoFactorConfirmationByUserId(
-        existingUser.id
+    if (!existingUser.emailVerified) {
+      const { email, token } = await generateVerificationToken(
+        existingUser.email
       );
 
-      if (existingConfirmation) {
-        await db.twoFactorConfirmation.delete({
-          where: { id: existingConfirmation.id },
-        });
-      }
-
-      await db.twoFactorConfirmation.create({
-        data: {
-          userId: existingUser.id,
-        },
-      });
-    } else {
-      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
-      await sendTwoFactorTokenEmail(twoFactorToken.email, twoFactorToken.token);
-      return { twoFactor: true };
+      await sendVerificationEmail(email, token);
+      return { success: "Confirmation email sent!" };
     }
-  }
 
-  try {
+    if (existingUser.isTwoFactorEnabled && existingUser.email) {
+      if (code) {
+        const twoFactorToken = await getTwoFactorTokenByEmail(
+          existingUser.email
+        );
+
+        if (!twoFactorToken || twoFactorToken.token !== code)
+          return { error: "Invalid code!" };
+
+        const hasExpired = new Date(twoFactorToken.expiresAt) < new Date();
+
+        if (hasExpired)
+          return {
+            error: "Code expired!",
+          };
+
+        await db.twoFactorToken.delete({
+          where: {
+            id: twoFactorToken.id,
+          },
+        });
+
+        const existingConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (existingConfirmation) {
+          await db.twoFactorConfirmation.delete({
+            where: { id: existingConfirmation.id },
+          });
+        }
+
+        await db.twoFactorConfirmation.create({
+          data: {
+            userId: existingUser.id,
+          },
+        });
+      } else {
+        const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+        await sendTwoFactorTokenEmail(
+          twoFactorToken.email,
+          twoFactorToken.token
+        );
+        return { twoFactor: true };
+      }
+    }
+
     // Disable redirect for this function. signIn uses next redirect and next redirect cannot be used within try catch
     await signIn("credentials", {
       email,
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,5 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-type Props = {};
-
-export default async function SettingsPage({}: Props) {
-  const session = await auth();
-  return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/auth/login" });
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
-    </div>
-  );
+export default function SettingsPage() {
+  return <div>Settings page</div>;
 }
